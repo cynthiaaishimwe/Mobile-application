2f4157b10a7c8aad444b59cb2524ae9bf1d4630d
cynthiaaishimwe <ishimwecynthiaa@gmail.com>
2023-02-03 08:36:47 +0300

    studentrecords

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    var name = "kethia"
+    var age = "21"
+    var phone = "0788888"
+    var weight = "54kg"
+    var citizenship = "not kenyan"
+    println(name)
+    println(age)
+    println(phone)
+    println(weight)
+    println(citizenship)
+
+
+}
